0b6a620b3b38bcbe5fe4dc6845524d186a9cc756
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-03-29 20:49:11 +0600

    problem 3 add about gcd formula

---
+++ Problem_solving_module_part_two/problem-3.js
@@ -0,0 +1,15 @@
+'use strict';
+console.log('working...');
+
+/*
+
+3. Write a JavaScript program to get the greatest common divisor (GCD) of two positive integers.
+
+Two positive integers: 81 and 153
+GCD= 9
+
+*/
+
+// point 1 : what is the greatest common divisor(GCD) solution : the greatest common factor number that divides them,
+
+// point 2 : how does gcd calculated : solution :
