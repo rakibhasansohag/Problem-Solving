fde64f3d2c7dbcd83ff5151654372a7c3e7eb072
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-04-10 23:54:17 +0600

    add day 13 sumNumbrs of a string and return sum of all numbers

---
+++ 30_days-problem/day-13.js
@@ -0,0 +1,17 @@
+'use strict';
+console.log('working....');
+
+/*
+Write a javaScript function sumNumbers that takes a string. And find all numbers from this string and return the sum of those numbers.
+
+Example 2:
+Input: ‘‘afdafdf5fdfdf5fdfdf4fdfdf4fd’’
+Output: 18
+Example 2:
+Input: “abc1dfddf4djfdrfdf8fdfd9”
+Output: 22
+
+Constraints: 
+Number is not greater than or equal 10.
+
+*/
