71047093d3457b736d028cfab71d4fc95fc61a98
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-04-11 19:40:36 +0600

    solve takes a string and return sum of those numbers

--- 30_days-problem/day-13.js
+++ 30_days-problem/day-13.js
@@ -15,3 +15,24 @@ Constraints:
 Number is not greater than or equal 10.
 
 */
+
+// point : solution 1 : without using built - in
+const sumNumbers = (str) => {
+	let sum = 0;
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] >= 0) {
+			sum += parseInt(str[i]);
+		}
+	}
+	return sum;
+};
+
+console.log(sumNumbers('afdafdf5fdfdf5fdfdf4fdfdf4fd')); /// 18
+
+console.log(sumNumbers('abc1dfddf4djfdrfdf8fdfd9')); /// 22
+
+console.log(sumNumbers('12dfds34fsdf56fsdf78dsfds9')); /// 45
+
+console.log(
+	'///////////////////////////////////////////////////////////////////////////////',
+);
