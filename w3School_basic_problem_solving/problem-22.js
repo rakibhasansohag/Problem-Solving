6c11c12c8217197fc5b151d4e681178fcf2bdb46
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-06-06 10:38:49 +0600

    add problem 22 : Create a new string from a given string taking the last 3 characters and added at both the front and back

---
+++ w3School_basic_problem_solving/problem-22.js
@@ -0,0 +1,7 @@
+'use strict '
+
+console.log('working....')
+
+// task 22 : Create a new string from a given string taking the last 3 characters and added at both the front and back.
+
+// point : solution 1
