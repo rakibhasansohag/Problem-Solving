d27d25611c9f8d1b8bd5b8ca000ca683e476f9e6
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-06-06 09:32:22 +0600

    problem solve 17: Remove a character at the specified position of a given string and return the new string

--- w3School_basic_problem_solving/problem-17.js
+++ w3School_basic_problem_solving/problem-17.js
@@ -2,3 +2,25 @@
 console.log('working...');
 
 // task 17 : Remove a character at the specified position of a given string and return the new string
+
+// point : solution 1
+
+function removeCharacter(str, position) {
+	let firstPart = str.substring(0, position);
+	let lastPart = str.substring(position + 1, str.length);
+	return firstPart + lastPart;
+}
+
+console.log(removeCharacter('Python', 0));
+console.log(removeCharacter('Python', 3));
+
+// point : solution 2 (using split and splice)
+
+function removeCharacter2(str, position) {
+	let arr = str.split('');
+	arr.splice(position, 1);
+	return arr.join('');
+}
+console.log(`....................................................`);
+console.log(removeCharacter2('Python', 0));
+console.log(removeCharacter2('Python', 3));
