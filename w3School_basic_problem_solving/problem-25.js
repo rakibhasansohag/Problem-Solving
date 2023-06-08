cd325861e157da7999d318700154b9065bfda072
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-06-08 10:35:30 +0600

    problem solve 25 : problem 25 : Find the largest of three given integers

--- w3School_basic_problem_solving/problem-25.js
+++ w3School_basic_problem_solving/problem-25.js
@@ -2,3 +2,66 @@
 console.log('working....');
 
 // task : problem 27 : Find the largest of three given integers
+
+// point : solution 1
+
+function maxOfThree(x, y, z) {
+	let maxValue = 0;
+
+	if (x > y) {
+		maxValue = x;
+	} else {
+		maxValue = y;
+	}
+
+	if (z > maxValue) {
+		maxValue = z;
+	}
+
+	return maxValue;
+}
+
+console.log(maxOfThree(1, 2, 3));
+console.log(maxOfThree(0, -110, -10));
+
+// point : solution 2
+
+function maxOfThree2(x, y, z) {
+	let maxValue = 0;
+
+	if (x > y && x > z) {
+		maxValue = x;
+	} else if (y > x && y > z) {
+		maxValue = y;
+	} else {
+		maxValue = z;
+	}
+	return maxValue;
+}
+
+console.log(maxOfThree2(1, 2, 3));
+console.log(maxOfThree2(0, -110, -10));
+
+// point : solution 3
+
+function maxOfThree3(x, y, z) {
+	let maxValue = 0;
+
+	if (x > y) {
+		if (x > z) {
+			maxValue = x;
+		} else {
+			maxValue = z;
+		}
+	} else {
+		if (y > z) {
+			maxValue = y;
+		} else {
+			maxValue = z;
+		}
+	}
+	return maxValue;
+}
+
+console.log(maxOfThree3(1, 2, 3));
+console.log(maxOfThree3(0, -110, -10));
