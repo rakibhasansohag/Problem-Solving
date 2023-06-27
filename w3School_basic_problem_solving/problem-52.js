7ed3f91eb599937b8b347cea5351a146b97ae5ce
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-06-27 12:53:01 +0600

    problem solve : Write a JavaScript program to check whether a matrix is a diagonal matrix or not. In linear algebra, a diagonal matrix is a matrix in which the entries outside the main diagonal are all zero (the diagonal from the upper left to the lower right)

--- w3School_basic_problem_solving/problem-52.js
+++ w3School_basic_problem_solving/problem-52.js
@@ -57,3 +57,84 @@ function isSentenceCorrect2(str) {
 console.log(isSentenceCorrect2('This is an example of *correct* sentence.')); /// true
 console.log(isSentenceCorrect2('this is an example of *correct* sentence.')); /// false
 console.log(isSentenceCorrect2('This is an example of *correct* sentence')); /// false
+
+console.log('------------------ another problem ------------------');
+
+// task : Given a string, find the number of different characters in it.
+
+// point : solution 1 ( for loop )
+
+function differentSymbolsNaive(str) {
+	let result = [];
+	for (let i = 0; i < str.length; i++) {
+		if (!result.includes(str[i])) {
+			result.push(str[i]);
+		}
+	}
+	return result.length;
+}
+
+console.log(differentSymbolsNaive('cabca')); /// 3
+console.log(differentSymbolsNaive('aba')); /// 2
+console.log(differentSymbolsNaive('c')); /// 1
+
+// task : Write a JavaScript program to check whether a matrix is a diagonal matrix or not. In linear algebra, a diagonal matrix is a matrix in which the entries outside the main diagonal are all zero (the diagonal from the upper left to the lower right)
+
+// point : exercise Example:
+// [1, 0, 0], [0, 2, 0], [0, 0, 3] ]) = true
+// [1, 0, 0], [0, 2, 3], [0, 0, 3] ]) = false
+
+// point : solution 1 ( nested for loop with 2d  array condition )
+
+function isDiagonalMatrix(matrix) {
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = 0; j < matrix.length; j++) {
+			if (i !== j && matrix[i][j] !== 0) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+console.log(
+	isDiagonalMatrix([
+		[1, 0, 0],
+		[0, 2, 0],
+		[0, 0, 3],
+	]),
+); /// true
+
+console.log(
+	isDiagonalMatrix([
+		[1, 0, 0],
+		[0, 2, 3],
+		[0, 0, 3],
+	]),
+); /// false
+
+// error : solution 2 ( nested for loop with 1d array condition )  not gonna work until we use 2d array and nested array
+
+function isDiagonalMatrix2(matrix) {
+	for (let i = 0; i < matrix.length; i++) {
+		if (matrix[i][i] === 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+console.log(
+	isDiagonalMatrix2([
+		[1, 0, 0],
+		[0, 2, 0],
+		[0, 0, 3],
+	]),
+); /// true
+console.log(
+	isDiagonalMatrix2([
+		[1, 0, 0],
+		[0, 2, 3],
+		[0, 0, 3],
+	]),
+); /// false
