425b8a117e272356ee9644270bf6e65646e4498b
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-07-02 20:21:26 +0600

    problem solve : Create an array of prefix sums of the given array

--- w3School_basic_problem_solving/problem-56.js
+++ w3School_basic_problem_solving/problem-56.js
@@ -134,3 +134,51 @@ console.log(evenDigits1(1020)); /// 3
 console.log(evenDigits1(102)); /// 2
 
 console.timeEnd('evenDigits2');
+
+console.log('---------------------- another problem ----------------------');
+/*
+task : Write a JavaScript program to create an array of prefix sums of the given array.
+
+   : In computer science, the prefix sum, cumulative sum, inclusive scan, or simply scan of a sequence of numbers x0, x1, x2, ... is a second sequence of numbers y0, y1, y2, ..., the sums of prefixes of the input sequence:
+y0 = x0
+y1 = x0 + x1
+y2 = x0 + x1+ x2
+
+point : solution 1 ( nested  for loop )
+*/
+
+console.time('prefixSums1');
+
+function prefixSums(array) {
+	let result = [];
+
+	for (let i = 0; i < array.length; i++) {
+		result[i] = 0;
+
+		for (let j = 0; j < i + 1; j++) {
+			result[i] += array[j];
+		}
+	}
+	return result;
+}
+
+console.log(prefixSums([1, 2, 3, 4, 5])); /// [1, 3, 6, 10, 15]
+console.log(prefixSums([1, 2, -3, 4, 5])); /// [1, 3, 0, 4, 9]
+
+console.timeEnd('prefixSums1');
+
+// point : solution 2 ( reduce method )
+
+console.time('prefixSums2');
+
+function prefixSums1(array) {
+	return array.reduce((result, item, index) => {
+		result[index] = (result[index - 1] || 0) + item;
+		return result;
+	}, []);
+}
+
+console.log(prefixSums1([1, 2, 3, 4, 5])); /// [1, 3, 6, 10, 15]
+console.log(prefixSums1([1, 2, -3, 4, 5])); /// [1, 3, 0, 4, 9]
+
+console.timeEnd('prefixSums2');
