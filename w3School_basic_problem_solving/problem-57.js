a2543ea13c1d67330d814b4794742b7cf7f44667
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-07-03 19:38:02 +0600

    problem solve For : : Find all distinct prime factors of a given integer

--- w3School_basic_problem_solving/problem-57.js
+++ w3School_basic_problem_solving/problem-57.js
@@ -54,3 +54,68 @@ console.log(properFraction2(12, 13));
 console.log(properFraction2(12, 30));
 
 console.timeEnd('SOLUTION :: 2');
+
+console.log('-------------------- another problem ---------------------');
+// task : Find all distinct prime factors of a given integer
+
+// point : SOLUTION : 1 ( using nested loop )
+
+console.time('SOLUTION :: 1');
+
+const primeFactors = (num) => {
+	function isPrime(num) {
+		for (let i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i === 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	let result = [];
+
+	for (let i = 2; i <= num; i++) {
+		while (isPrime(i) && num % i === 0) {
+			if (!result.includes(i)) {
+				result.push(i);
+			}
+
+			num = num / i;
+		}
+	}
+
+	return result;
+};
+
+console.log(primeFactors(100)); // [2, 5]
+console.log(primeFactors(101)); // [101]
+console.log(primeFactors(103)); // [103]
+console.log(primeFactors(104)); // [2, 13]
+console.log(primeFactors(105)); // [3, 5, 7]
+console.timeEnd('SOLUTION :: 1');
+
+// point : SOLUTION : 2 ( using recursion  )
+
+console.time('SOLUTION :: 2');
+
+const primeFactors2 = (num) => {
+	let result = [];
+
+	for (let i = 2; i <= num; i++) {
+		while (num % i === 0) {
+			if (!result.includes(i)) {
+				result.push(i);
+			}
+
+			num = num / i;
+		}
+	}
+
+	return result;
+};
+
+console.log(primeFactors2(100)); // [2, 5]
+console.log(primeFactors2(101)); // [101]
+console.log(primeFactors2(103)); // [103]
+console.log(primeFactors2(104)); // [2, 13]
+
+console.timeEnd('SOLUTION :: 2');
