b6087e7118930cef6a688fdc217deff646b8f77d
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-07-11 18:17:33 +0600

    problem solve : Write a JavaScript program to convert a comma-separated value (CSV) string to a 2D array of objects. The first row of the string is used as the title row.

--- w3School_Fundamental_Part_1/problem-2.js
+++ w3School_Fundamental_Part_1/problem-2.js
@@ -42,3 +42,34 @@ console.log(csToArray2('head1,head2\na,b\nc,d', ',', true)); // [['a','b'],['c',
 
 console.log('------------------ another problem ------------------');
 
+/*
+
+todo : Write a JavaScript program to convert a comma-separated value (CSV) string to a 2D array of objects. The first row of the string is used as the title row.
+
+$ Use Array.prototype.slice() and Array.prototype.indexOf('\n') and String.prototype.split(delimiter) to separate the first row (title row) into values.
+
+$ Use String.prototype.split('\n') to create a string for each row, then Array.prototype.map() and String.prototype.split(delimiter) to separate the values in each row.
+
+$ Use Array.prototype.reduce() to create an object for each row's values, with the keys parsed from the title row.
+Omit the second argument, delimiter, to use a default delimiter of.
+
+
+*/
+
+const csToArrayOfObject = (data, delimiter = ',') => {
+	const titles = data.slice(0, data.indexOf('\n')).split(delimiter);
+
+	return data
+		.slice(data.indexOf('\n') + 1)
+		.split('\n')
+		.map((el) => {
+			const values = el.split(delimiter);
+			return titles.reduce(
+				(obj, title, index) => ((obj[title] = values[index]), obj),
+				{},
+			);
+		});
+};
+
+console.log(csToArrayOfObject('col1,col2\na,b\nc,d')); // [{'col1': 'a', 'col2': 'b'}, {'col1': 'c', 'col2': 'd'}];
+console.log(csToArrayOfObject('col1;col2\na;b\nc;d', ';')); // [{'col1': 'a', 'col2': 'b'}, {'col1': 'c', 'col2': 'd'}];
