1716d8fec984a452d373213f056fe8634ada80d9
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-07-20 10:51:11 +0600

    problem solve : Filter out the specified values from a specified array

--- w3School_Fundamental_Part_1/problem-3.js
+++ w3School_Fundamental_Part_1/problem-3.js
@@ -68,7 +68,78 @@ const dig2 = (obj, target) => {
 	return undefined;
 };
 
-console.log(dig2(data, 'some data')); // 'some data'
-console.log(dig2(data, 'level3')); // 'some data'
-console.log(dig2(data, 'level4')); // undefined
-console.log(dig2(dog, 'status')); // 'success'
+// console.log(dig2(data, 'some data')); // 'some data'
+// console.log(dig2(data, 'level3')); // 'some data'
+// console.log(dig2(data, 'level4')); // undefined
+// console.log(dig2(dog, 'status')); // 'success'
+
+console.log('...................another problem...........');
+
+// todo : Converts a specified number to an array of digits.
+
+// point : ( solution 1 : using spread operator and map method )
+
+const digitize = (n) => [...`${n}`].map((i) => parseInt(i));
+
+// console.log(digitize(123)); // [1, 2, 3]
+// console.log(digitize(1230)); // [1, 2, 3, 0]
+// console.log(digitize(8675309)); // [8, 6, 7, 5, 3, 0, 9]
+
+// point : ( solution 2 : for loop and push method )
+
+const digitize2 = (n) => {
+	const result = [];
+
+	for (const i of `${n}`) {
+		result.push(parseInt(i));
+	}
+	return result;
+};
+
+// console.log(digitize2(123)); // [1, 2, 3]
+// console.log(digitize2(1230)); // [1, 2, 3, 0]
+// console.log(digitize2(8675309)); // [8, 6, 7, 5, 3, 0, 9]
+
+// point : ( solution 3 : using for loop and unshift method (  reversing the total result ) )
+
+const digitize3 = (n) => {
+	const result = [];
+
+	for (const i of `${n}`) {
+		result.unshift(parseInt(i));
+	}
+	return result;
+};
+
+console.log(digitize3(123)); // [1, 2, 3]
+console.log(digitize3(1230)); // [1, 2, 3, 0]
+console.log(digitize3(8675309)); // [8, 6, 7, 5, 3, 0, 9]
+
+console.log('...................another problem...........');
+
+// todo : Filter out the specified values from a specified array.
+
+// point : ( solution 1 : using filter method )
+
+const pull = (arr, ...args) => arr.filter((v) => !args.includes(v));
+
+// console.log(pull(['a', 'b', 'c', 'a', 'b', 'c'], 'a', 'c')); // [ 'b', 'b' ]
+// console.log(pull(['a', 'b', 'c', 'a', 'b', 'c'], 'b')); // [ 'a', 'c', 'a', 'c' ]
+
+// point : ( solution 2 : using for loop and push method )
+
+const pull2 = (arr, ...args) => {
+	const result = [];
+
+	for (const i of arr) {
+		if (!args.includes(i)) {
+			result.push(i);
+		}
+	}
+	return result;
+};
+
+console.log(pull2(['a', 'b', 'c', 'a', 'b', 'c'], 'a', 'c')); // [ 'b', 'b' ]
+console.log(pull2(['a', 'b', 'c', 'a', 'b', 'c'], 'b')); // [ 'a', 'c', 'a', 'c' ]
+
+
