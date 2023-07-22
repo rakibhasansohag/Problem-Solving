27868781c38c94c25303248d238d53aef2eb899f
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-07-22 14:13:42 +0600

    problem solve : Returns true if the provided predicate function returns true for all elements in a collection, false otherwise

--- w3School_Fundamental_Part_1/problem-5.js
+++ w3School_Fundamental_Part_1/problem-5.js
@@ -22,15 +22,35 @@ const minMax2 = (arr, comparator = (a, b) => a - b) => {
 	return arr.reduce((a, b) => (comparator(a, b) >= 0 ? b : a));
 };
 
-console.log(minMax2([1, 3, 2]));
-console.log(minMax2([10, 30, 20], (a, b) => b - a));
-console.log(
-	minMax2(
-		[
-			{ name: 'Kevin', age: 16 },
-			{ name: 'John', age: 20 },
-			{ name: 'Ani', age: 19 },
-		],
-		(a, b) => a.age - b.age,
-	),
-);
+// console.log(minMax2([1, 3, 2]));
+// console.log(minMax2([10, 30, 20], (a, b) => b - a));
+// console.log(
+// 	minMax2(
+// 		[
+// 			{ name: 'Kevin', age: 16 },
+// 			{ name: 'John', age: 20 },
+// 			{ name: 'Ani', age: 19 },
+// 		],
+// 		(a, b) => a.age - b.age,
+// 	),
+// );
+
+console.log('------------- another problem -----------------');
+
+// todo : Returns true if the provided predicate function returns true for all elements in a collection, false otherwise.
+
+// point : solution 1  ( using every() )
+
+const all = (arr, fn = Boolean) => arr.every(fn);
+
+console.log(all([4, 2, 3], (x) => x > 1));
+console.log(all([4, 2, 3], (x) => x < 1));
+console.log(all([1, 2, 3]));
+
+// point : solution 2  ( using reduce() )
+
+const all2 = (arr, fn = Boolean) => arr.reduce((a, b) => a && fn(b), true);
+
+console.log(all2([4, 2, 3], (x) => x > 1));
+console.log(all2([4, 2, 3], (x) => x < 1));
+console.log(all2([1, 2, 3]));
