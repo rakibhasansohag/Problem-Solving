6af8c9bc201b28861098a90975dd3f9de4b59867
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-07-24 20:39:18 +0600

    problem solve : Get every nth element in a given array

--- w3School_Fundamental_Part_1/problem-6.js
+++ w3School_Fundamental_Part_1/problem-6.js
@@ -24,3 +24,84 @@ const extendHex2 = (shortHex) =>
 
 console.log(extendHex2('#03f')); // #0033ff
 console.log(extendHex2('05a')); // #0055aa
+
+console.log('--------------- another problem -----------------');
+
+// todo: Get every nth element in a given array.
+
+// point : solution 1 ( using filter() )
+const everyNth1 = (arr, nth) =>
+	arr.filter((_, index) => index % nth === nth - 1);
+
+console.log(everyNth1([1, 2, 3, 4, 5, 6], 1)); // [ 1,2,3,4,5,6 ]
+console.log(everyNth1([1, 2, 3, 4, 5, 6], 2)); // [ 2, 4, 6 ]
+console.log(everyNth1([1, 2, 3, 4, 5, 6], 3)); // [ 2, 4, 6 ]
+console.log(everyNth1([1, 2, 3, 4, 5, 6], 4)); // [ 2, 4, 6 ]
+
+// point : solution 2 ( using reduce() )
+const everyNth2 = (arr, nth) =>
+	arr.reduce(
+		(cur, element, index) =>
+			index % nth === nth - 1 ? [...cur, element] : cur,
+		[],
+	);
+
+console.log(everyNth2([1, 2, 3, 4, 5, 6], 1)); // [ 1,2,3,4,5,6 ]
+console.log(everyNth2([1, 2, 3, 4, 5, 6], 2)); // [ 2, 4, 6 ]
+console.log(everyNth2([1, 2, 3, 4, 5, 6], 3)); // [ 2, 4, 6 ]
+console.log(everyNth2([1, 2, 3, 4, 5, 6], 4)); // [ 2, 4, 6 ]
+
+// point : solution 3 ( using for loop )
+const everyNth3 = (arr, nth) => {
+	let result = [];
+
+	for (let i = 0; i < arr.length; i++) {
+		if (i % nth === nth - 1) {
+			result.push(arr[i]);
+		}
+	}
+	return result;
+};
+
+console.log(everyNth3([1, 2, 3, 4, 5, 6], 1)); // [ 1,2,3,4,5,6 ]
+console.log(everyNth3([1, 2, 3, 4, 5, 6], 2)); // [ 2, 4, 6 ]
+console.log(everyNth3([1, 2, 3, 4, 5, 6], 3)); // [ 2, 4, 6 ]
+console.log(everyNth3([1, 2, 3, 4, 5, 6], 4)); // [ 2, 4, 6 ]
+
+// point: solution 4 ( using forEach() )
+const everyNth4 = (arr, nth) => {
+	let result = [];
+
+	arr.forEach((element, index) => {
+		if (index % nth === nth - 1) {
+			result.push(element);
+		}
+	});
+
+	return result;
+};
+
+console.log(everyNth4([1, 2, 3, 4, 5, 6], 1)); // [ 1,2,3,4,5,6 ]
+console.log(everyNth4([1, 2, 3, 4, 5, 6], 2)); // [ 2, 4, 6 ]
+console.log(everyNth4([1, 2, 3, 4, 5, 6], 3)); // [ 2, 4, 6 ]
+console.log(everyNth4([1, 2, 3, 4, 5, 6], 4)); // [ 2, 4, 6 ]
+
+// point : solution 5 ( using for of loop )
+
+const everyNth5 = (arr, nth) => {
+	let result = [];
+
+	for (const [index, element] of arr.entries()) {
+		if (index % nth === nth - 1) {
+			result.push(element);
+		}
+	}
+	return result;
+};
+
+console.log(everyNth5([1, 2, 3, 4, 5, 6], 1)); // [ 1,2,3,4,5,6 ]
+console.log(everyNth5([1, 2, 3, 4, 5, 6], 2)); // [ 2, 4, 6 ]
+console.log(everyNth5([1, 2, 3, 4, 5, 6], 3)); // [ 2, 4, 6 ]
+console.log(everyNth5([1, 2, 3, 4, 5, 6], 4)); // [ 2, 4, 6 ]
+
+console.log('--------------- another problem -----------------');
