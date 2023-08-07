3d76aa21a5a3c08cafbf9311a3c4404ea1a32263
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-08-07 13:32:33 +0600

    probelm solve : Group the elements of a given array based on the given function

--- w3School_Fundamental_Part_1/problem-13.js
+++ w3School_Fundamental_Part_1/problem-13.js
@@ -90,3 +90,52 @@ const getURLParameters2 = (url) => {
 console.log(getURLParameters2('http://url.com/page?name=Adam&surname=Smith'));
 console.log(getURLParameters2('google.com'));
 console.log(getURLParameters2('https://www.w3resource.com'));
+
+console.log('------------------ another problem ------------------');
+
+// task : Group the elements of a given array based on the given function.
+
+// point solution 1  - using Array.reduce() and Array.push()
+
+const groupBy = (arr, fn) => {
+	return arr.reduce((acc, cur) => {
+		const key = fn(cur);
+		if (!acc[key]) {
+			acc[key] = [];
+		}
+		acc[key].push(cur);
+		return acc;
+	}, {});
+};
+
+console.log(groupBy([6.1, 4.2, 6.3], Math.sqrt));
+console.log(groupBy([6.1, 4.2, 6.3], Math.floor));
+console.log(groupBy(['one', 'two', 'three'], (i) => i.length));
+
+// point solution 2  - using Array.reduce() and Array.concat()
+
+const groupBy2 = (arr, fn) => {
+	return arr.reduce((acc, cur) => {
+		const key = fn(cur);
+		acc[key] = acc[key] || [];
+		return (acc[key] = acc[key].concat(cur));
+	}, {});
+};
+
+console.log(groupBy2([6.1, 4.2, 6.3], Math.sqrt));
+console.log(groupBy2([6.1, 4.2, 6.3], Math.floor));
+console.log(groupBy2(['one', 'two', 'three'], (i) => i.length));
+
+// point solution 3  - using w3resource solution
+
+const groupBy3 = (arr, fn) =>
+	arr
+		.map(typeof fn === 'function' ? fn : (val) => val[fn])
+		.reduce((acc, val, i) => {
+			acc[val] = (acc[val] || []).concat(arr[i]);
+			return acc;
+		}, {});
+
+console.log(groupBy3([6.1, 4.2, 6.3], Math.sqrt));
+console.log(groupBy3([6.1, 4.2, 6.3], Math.floor));
+console.log(groupBy3(['one', 'two', 'three'], (i) => i.length));
