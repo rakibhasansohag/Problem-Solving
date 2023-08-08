abb5d627ef5b03eab63750a7696774d61f2d35aa
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-08-08 20:30:12 +0600

    problem solve : Join all given URL segments together, then normalizes the resulting URL

--- w3School_Fundamental_Part_1/problem-14.js
+++ w3School_Fundamental_Part_1/problem-14.js
@@ -47,3 +47,101 @@ const twoDimensionArray3 = (width, height, value) => {
 console.log(twoDimensionArray3(2, 2, 0));
 console.log(twoDimensionArray3(3, 3, 3));
 console.log(twoDimensionArray3(3, 3, 'x'));
+
+console.log('------------------ another problem ------------------');
+
+// task : Initialize an array containing the numbers in the specified range where start and end are inclusive with their common difference step.
+
+// point 1 solution : using for loop
+
+const initializeArrayWithRange = (end, start = 0, step = 1) => {
+	let arr = [];
+
+	for (let i = start; i <= end; i += step) {
+		arr.push(i);
+	}
+	return arr;
+};
+
+console.log(initializeArrayWithRange(5));
+console.log(initializeArrayWithRange(8, 3));
+console.log(initializeArrayWithRange(6, 0, 2));
+
+// point 2 solution : using while loop
+
+const initializeArrayWithRange2 = (end, start = 0, step = 1) => {
+	let arr = [];
+
+	while (start <= end) {
+		arr.push(start);
+		start += step;
+	}
+	return arr;
+};
+
+console.log(initializeArrayWithRange2(5));
+console.log(initializeArrayWithRange2(8, 3));
+console.log(initializeArrayWithRange2(6, 0, 2));
+
+// point 3 solution : using recursion
+
+const initializeArrayWithRange3 = (end, start = 0, step = 1) => {
+	if (start > end) return [];
+
+	return [start, ...initializeArrayWithRange3(end, start + step, step)];
+};
+
+console.log(initializeArrayWithRange3(5));
+console.log(initializeArrayWithRange3(8, 3));
+console.log(initializeArrayWithRange3(6, 0, 2));
+
+// point 4 solution : using Array.from()
+
+const initializeArrayWithRange4 = (end, start = 0, step = 1) => {
+	return Array.from(
+		{ length: Math.ceil((end - start + 1) / step) },
+		(v, i) => i * step + start,
+	);
+};
+
+console.log(initializeArrayWithRange4(5));
+console.log(initializeArrayWithRange4(8, 3));
+console.log(initializeArrayWithRange4(6, 0, 2));
+
+console.log('------------------ another problem ------------------');
+
+// task : Join all given URL segments together, then normalizes the resulting URL.
+
+// point 1 solution : using reduce()
+
+const joinURLSegments = (...args) => {
+	return args.reduce((acc, cur) => {
+		return acc.replace(/\/+$/, '') + '/' + cur.replace(/^\/+/, '');
+	});
+};
+
+console.log(
+	joinURLSegments('http://', 'www.google.com', '/a', '/b/cd', '?foo=123'),
+);
+
+// point 2 solution : using Array.join() and String.replace()
+
+const joinURLSegments2 = (...args) => {
+	return args.join('/').replace(/[\/]+/g, '/');
+};
+
+console.log(
+	joinURLSegments2('http://', 'www.google.com', '/a', '/b/cd', '?foo=123'),
+);
+
+// point 3 solution : solving with URL object
+
+const joinURLSegments3 = (...args) => {
+	return new URL(args.join('/')).href;
+};
+
+console.log(
+	joinURLSegments3('http://', 'www.google.com', '/a', '/b/cd', '?foo=123'),
+);
+
+console.log('------------------ another problem ------------------');
