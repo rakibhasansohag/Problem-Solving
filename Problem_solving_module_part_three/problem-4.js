051f73c751918b190a9276f5919b1874087adb7a
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-08-11 01:21:19 +0600

    problem solve : remove the spacs found in a a string

--- Problem_solving_module_part_three/problem-4.js
+++ Problem_solving_module_part_three/problem-4.js
@@ -1,1 +1,26 @@
+'use strict';
+console.log('working.......');
 // task : remove the spaces found in a string .
+
+// point : solution 1 : with out using any built in function
+
+const removeSpace = (str) => {
+	let newStr = '';
+
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] !== ' ') {
+			newStr += str[i];
+		}
+	}
+	return newStr;
+};
+
+console.log(removeSpace('    hello world      :) '));
+console.log(removeSpace('    hello world      :) ').length);
+
+// point : solution 2 : with using built in function ( regular expression and replace function )
+
+const removeSpace2 = (str) => str.replace(/\s/g, '');
+
+console.log(removeSpace2('    hello world      :) '));
+console.log(removeSpace2('    hello world      :) ').length);
