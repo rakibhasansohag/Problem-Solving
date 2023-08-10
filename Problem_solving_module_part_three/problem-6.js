9d08860d0817e22eb2e30419d3a6cfc0a4b1daff
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-08-11 01:28:45 +0600

    problem solve : calculate the sum of numbers within an array .

--- Problem_solving_module_part_three/problem-6.js
+++ Problem_solving_module_part_three/problem-6.js
@@ -1,1 +1,23 @@
+'use strict ';
+console.log('working.......');
+
 // task : calculate the sum of numbers within an array .
+
+// point : solution 1 : with out using any built in function
+
+const sumOfArray = (arr) => {
+	let sum = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+	return sum;
+};
+
+console.log(sumOfArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); /// 55
+
+// point : solution 2 : with using reduce function
+
+const sumOfArray2 = (arr) => arr.reduce((acc, cur) => acc + cur, 0);
+
+console.log(sumOfArray2([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); /// 55
