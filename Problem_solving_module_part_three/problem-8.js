d648f869435f28c0d8164f24f77322c5d131d5b5
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-08-11 01:40:45 +0600

    problem solve for start pattern

--- Problem_solving_module_part_three/problem-8.js
+++ Problem_solving_module_part_three/problem-8.js
@@ -1,3 +1,6 @@
+'use strict ';
+console.log('working.......');
+
 // task :  Write a javascript program that prints this pattern below .
 
 /*
@@ -15,3 +18,38 @@
 ****
 
 */
+
+// point : problem x :
+
+console.log('*');
+console.log('**');
+console.log('***');
+console.log('****');
+console.log('*****' + '\n');
+
+// point : solution 1 : with out using any built in function
+
+const printPattern = (n) => {
+	let str = '';
+
+	for (let i = 1; i <= n; i++) {
+		for (let j = 1; j <= i; j++) {
+			str += '*';
+		}
+
+		str += '\n';
+	}
+	return str;
+};
+
+console.log(printPattern(5));
+
+// point : solution 1 : using built in function (repeat)
+
+const printPattern2 = (n) => {
+	for (let i = 1; i <= n; i++) {
+		console.log('*'.repeat(i));
+	}
+};
+
+printPattern2(5);
