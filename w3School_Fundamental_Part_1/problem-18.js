efe5d26af1f35beda0eae0a845417bd46ab2b3d9
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-08-18 13:00:11 +0600

    problem solve : Group the elements of an array based on the given function and returns the count of elements in each group

--- w3School_Fundamental_Part_1/problem-18.js
+++ w3School_Fundamental_Part_1/problem-18.js
@@ -20,3 +20,20 @@ const average = converge(
 console.log(average([6, 7])); // 6.5
 console.log(average([1, 2, 3, 4, 5, 6, 7, 8])); // 4.5
 console.log(average([1, 2, 3, 4, 5, 6, 7, 8, 9])); // 5
+
+// task : Group the elements of an array based on the given function and returns the count of elements in each group.
+
+// point : solution 1 : using reduce
+
+const countBy = (arr, fn) => {
+	return arr
+		.map(typeof fn === 'function' ? fn : (val) => val[fn])
+		.reduce((acc, val) => {
+			acc[val] = (acc[val] || 0) + 1;
+			return acc;
+		}, {});
+};
+
+console.log(countBy([6.1, 4.2, 6.3], Math.floor)); // {4: 1, 6: 2}
+console.log(countBy(['one', 'two', 'three'], 'length')); // {3: 2, 5: 1}
+console.log(countBy([5.13, 3.4, 4.4], Math.sqrt));
