6b9fe55156c44adfeb21578e04c74428443e84a3
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-08-25 01:59:08 +0600

    added probelm : Count the occurrences of a value in an array.

---
+++ w3School_Fundamental_Part_1/problem-19.js
@@ -0,0 +1,23 @@
+'use strict';
+console.log('working...');
+
+// task : Count the occurrences of a value in an array.
+
+/* 
+
+point :::: note :
+
+/ 1 Use recursion.
+
+/ 2 Check if the passed object is null and, if so, return null.
+
+/ 3 Use Object.assign() and an empty object ({}) to create a shallow clone of the original.
+
+/ 4 Use Object.keys() and Array.prototype.forEach() to determine which key-value pairs need to be deep cloned.
+
+/ 5 If the object is an Array, set the clone's length to that of the original and use Array.from(clone) to create a clone.
+
+*/
+
+// point : solution 1 : using recursion
+
