eb7ceff9589fefb63c76dbb022b92fea5579d3c1
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-09-25 11:18:20 +0600

    problem solve : Get the maximum value of an array, after mapping each element to a value using the provided function

--- w3School_Fundamental_Part_1/problem-25.js
+++ w3School_Fundamental_Part_1/problem-25.js
@@ -33,3 +33,31 @@ const musk3 = (cc, num = 4, mask = '*') => {
 console.log(musk3('123456789', 4));
 console.log(musk3('123456789', 3));
 console.log(musk3('123456789', 3, '$'));
+
+console.log('........... another Problem ...........');
+
+// task : Get the maximum value of an array, after mapping each element to a value using the provided function.
+
+// Point : solution 1 : using Math.max and spread operator
+
+const maxBy = (arr, fn) =>
+	Math.max(...arr.map(typeof fn === 'function' ? fn : (val) => val[fn]));
+
+console.log(maxBy([{ n: 4 }, { n: 2 }, { n: 8 }, { n: 6 }], (o) => o.n));
+console.log(maxBy([{ n: 4 }, { n: 2 }, { n: 8 }, { n: 6 }], 'n'));
+
+// Point : solution 2 : using reduce method
+
+const maxBy2 = (arr, fn) => {
+	if (arr.length === 0) return undefined;
+
+	return arr.reduce(
+		(acc, val) => {
+			return Math.max(acc, typeof fn === 'function' ? fn(val) : val[fn]);
+		},
+		typeof fn === 'function' ? fn(arr[0]) : arr[0][fn],
+	);
+};
+
+console.log(maxBy2([{ n: 4 }, { n: 2 }, { n: 8 }, { n: 6 }], (o) => o.n));
+console.log(maxBy2([{ n: 4 }, { n: 2 }, { n: 8 }, { n: 6 }], 'n'));
