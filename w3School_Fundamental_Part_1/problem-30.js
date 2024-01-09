f60477ea769aafe21573335d9115b1c1f831ef8b
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-01-09 16:52:58 +0600

    problem solve : Write a JavaScript program to pick  the key-value pairs corresponding to the given keys from an object.

--- w3School_Fundamental_Part_1/problem-30.js
+++ w3School_Fundamental_Part_1/problem-30.js
@@ -39,3 +39,50 @@ const fn1 = () => console.log('! delegator function');
 console.log(turnOn1(document.body, 'click', fn1));
 console.log(turnOn1(document.body, 'click', fn1, { target: 'p' }));
 console.log(turnOn1(document.body, 'click', fn1, { target: true }));
+
+console.log('----------------------------------------------------------------');
+
+// task : Write a JavaScript program to pick  the key-value pairs corresponding to the given keys from an object.
+
+// Point : solution 1 : using reduce
+
+const keys = (obj, arr) =>
+	arr.reduce((acc, cur) => (cur in obj && (acc[cur] = obj[cur]), acc), {});
+
+console.log(keys({ a: 1, b: '2', c: 3 }, ['a', 'c']));
+console.log(keys({ a: 1, b: '2', c: 3, d: 3 }, ['a', 'd']));
+
+// Point : solution 2 : without using built-in functions and prototype.hasOwnProperty
+console.log('--------------------------------');
+
+const keys1 = (obj, arr) => {
+	const result = {};
+
+	for (let i = 0; i < arr.length; i++) {
+		const key = arr[i];
+
+		if (obj.hasOwnProperty(key)) {
+			result[key] = obj[key];
+		}
+	}
+	return result;
+};
+
+console.log(keys1({ a: 1, b: '2', c: 3 }, ['a', 'c']));
+console.log(keys1({ a: 1, b: '2', c: 3, d: 3 }, ['a', 'd']));
+
+// Point : solution 3 : using includes
+const keys2 = (obj, arr) => {
+	const result = {};
+
+	for (let key in obj) {
+		if (arr.includes(key)) {
+			result[key] = obj[key];
+		}
+	}
+
+	return result;
+};
+
+console.log(keys2({ a: 1, b: '2', c: 3 }, ['a', 'c']));
+console.log(keys2({ a: 1, b: '2', c: 3, d: 3 }, ['a', 'd']));
