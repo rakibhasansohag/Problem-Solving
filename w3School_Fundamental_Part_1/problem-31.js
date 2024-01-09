5d4f75a64c0f2ae255d595805ec0bce77f4f16d5
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-01-10 02:17:55 +0600

    probelm solve : Filter an array of objects based on a condition while also filtering out unspecified keys.

--- w3School_Fundamental_Part_1/problem-31.js
+++ w3School_Fundamental_Part_1/problem-31.js
@@ -57,3 +57,56 @@ console.log(keysPick2({ a: 1, b: '2', c: 3 }, (x) => typeof x === 'string'));
 console.log(
 	keysPick2({ a: 1, b: '2', c: 3, d: false }, (x) => typeof x === 'boolean'),
 );
+
+console.log('----------------------------------------------------------------');
+
+// task : Write a JavaScript program to filter an array of objects based on a condition while also filtering out unspecified keys.
+
+// Point : solution 1  : using filter, map, and reduce
+const filterObj = (data, keys, fn) =>
+	data.filter(fn).map((el) =>
+		keys.reduce((acc, key) => {
+			acc[key] = el[key];
+			return acc;
+		}, {}),
+	);
+
+const data = [
+	{
+		id: 1,
+		name: 'john',
+		age: 24,
+	},
+	{
+		id: 2,
+		name: 'mike',
+		age: 50,
+	},
+	{
+		id: 3,
+		name: 'rakib',
+		age: '60',
+	},
+];
+
+console.log(filterObj(data, ['id', 'name'], (item) => item.age > 24));
+
+const filterObj1 = (data, keys, fn) => {
+	const result = [];
+
+	for (let i = 0; i < data.length; i++) {
+		const item = data[i];
+		if (fn(item)) {
+			const filteredItem = {};
+			for (let j = 0; j < keys.length; j++) {
+				const key = keys[j];
+				filteredItem[key] = item[key];
+			}
+			result.push(filteredItem);
+		}
+	}
+
+	return result;
+};
+
+console.log(filterObj1(data, ['id', 'name'], (item) => item.age > 24));
