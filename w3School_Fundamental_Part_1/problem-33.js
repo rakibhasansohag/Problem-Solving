24932f8d86ce9653628876a443bc28ca7570c4c3
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-01-11 14:24:03 +0600

    problem solve : Write a JavaScript program to create an array of elements, grouped based on the position in the original array.

--- w3School_Fundamental_Part_1/problem-33.js
+++ w3School_Fundamental_Part_1/problem-33.js
@@ -24,3 +24,46 @@ const validProperties1 = (props, values) => {
 console.log(validProperties1(['a', 'b', 'c'], [1, 2]));
 console.log(validProperties1(['a', 'b'], [1, 2, 3]));
 console.log(validProperties1(['a', 'b', 'c'], [1, 2, 3]));
+
+console.log('----------------------------------------------------------------');
+
+// Task : Write a JavaScript program to create an array of elements, grouped based on the position in the original array.
+
+// Point : solution 1 : using math.max and apply()
+const grouped = (...arrays) => {
+	const maxLength = Math.max(...arrays.map((x) => x.length));
+
+	return Array.from({ length: maxLength }).map((_, i) => {
+		return Array.from({ length: arrays.length }, (_, j) => arrays[j][i]);
+	});
+};
+console.log(grouped(['a', 'b'], [1, 2], [true, false]));
+console.log(grouped(['a'], [1, 2], [true, false]));
+
+// Point : solution 2 without using built-in functions
+const grouped1 = (...args) => {
+	let maxLength = 0;
+
+	for (let i = 0; i < args.length; i++) {
+		if (args[i].length > maxLength) {
+			maxLength = args[i].length;
+		}
+	}
+
+	const result = [];
+
+	for (let i = 0; i < maxLength; i++) {
+		const group = [];
+
+		for (let j = 0; j < args.length; j++) {
+			group[j] = i < args[j].length ? args[j][i] : undefined;
+		}
+
+		result[i] = group;
+	}
+
+	return result;
+};
+
+console.log(grouped1(['a', 'b'], [1, 2], [true, false]));
+console.log(grouped1(['a'], [1, 2], [true, false]));
