775840eb6e2f8bdf6070b4bc932884325dfa6cf2
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-01-13 23:07:51 +0600

    probelm solve : Write a JavaScript program to create an array of elements, ungrouping the elements in an array produced by zip and applying the provided function.

--- w3School_Fundamental_Part_1/problem-34.js
+++ w3School_Fundamental_Part_1/problem-34.js
@@ -21,3 +21,68 @@ console.log(validateNumber1('200'));
 console.log(validateNumber1('10'));
 console.log(validateNumber1('abc'));
 console.log(validateNumber1('-.23432'));
+
+console.log('--------------------------------');
+// task : Write a JavaScript program to create an array of elements, ungrouping the elements in an array produced by zip and applying the provided function.
+
+const unzip = (arr, fn) =>
+	arr
+		.reduce(
+			(acc, val) => (val.forEach((v, i) => acc[i].push(v)), acc),
+			Array.from({
+				length: Math.max(...arr.map((x) => x.length)),
+			}).map((x) => []),
+		)
+		.map((val) => fn(...val));
+
+console.log(
+	unzip(
+		[
+			[1, 10, 100],
+			[2, 20, 200],
+		],
+		(...args) => args.reduce((acc, v) => acc + v, 0),
+	),
+);
+
+console.log(
+	unzip(
+		[
+			[1, 10, 100],
+			[81, 200, 500],
+		],
+		(...args) => args.reduce((acc, v) => acc + v, 0),
+	),
+);
+
+// Point : solution : 2 using different way to solve the problem (for loop)
+const unzip1 = (arr, fn) => {
+	const maxLength = Math.max(...arr.map((x) => x.length));
+
+	const result = [];
+
+	for (let i = 0; i < maxLength; i++) {
+		const values = arr.map((x) => x[i]);
+		result.push(fn(...values));
+	}
+	return result;
+};
+console.log(
+	unzip1(
+		[
+			[1, 10, 100],
+			[2, 20, 200],
+		],
+		(...args) => args.reduce((acc, v) => acc + v, 0),
+	),
+);
+
+console.log(
+	unzip1(
+		[
+			[1, 10, 100],
+			[81, 200, 500],
+		],
+		(...args) => args.reduce((acc, v) => acc + v, 0),
+	),
+);
