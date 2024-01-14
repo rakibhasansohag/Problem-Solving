7250eb2b423c1b67f0e30f4359f36aed805096ee
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-01-14 16:52:14 +0600

    problem solve : Write a JavaScript program to get the nth element of a given array.

--- w3School_Fundamental_Part_1/problem-35.js
+++ w3School_Fundamental_Part_1/problem-35.js
@@ -52,3 +52,47 @@ console.log(
 		(item) => item.id,
 	),
 );
+
+// task : Write a JavaScript program to get the nth element of a given array.
+console.log('----------------------------------------------------------------');
+
+const nthElement = (arr, n = 0) =>
+	(n > 0 ? arr.slice(n, n + 1) : arr.slice(n))[0] ||
+	'nothing found ==> try something else';
+
+console.log(nthElement(['a', 'b', 'c'], 1));
+console.log(nthElement(['a', 'b', 'c'], 2));
+console.log(nthElement(['a', 'b', 'c'], 4));
+
+// Point : solution 2 : without using built-in functions
+const nthElement1 = (arr, n = 0) => {
+	if (n >= 0) {
+		for (let i = 0; i < arr.length; i++) {
+			if (i === n) {
+				return arr[i];
+			}
+		}
+	}
+	return 'nothing found ==> try something else';
+};
+
+console.log(nthElement1(['a', 'b', 'c'], 1));
+console.log(nthElement1(['a', 'b', 'c'], 2));
+console.log(nthElement1(['a', 'b', 'c'], 4));
+
+// Point : solving this problem assuming there is a index number
+const nthElement2 = (arr, n = 0, index = 0) => {
+	if (index === n) {
+		return arr[index] !== undefined
+			? arr[index]
+			: 'nothing found ==> try something else';
+	}
+	if (index < arr.length) {
+		return nthElement(arr, n, index + 1);
+	}
+	return 'nothing found ==> try something else';
+};
+
+console.log(nthElement2(['a', 'b', 'c'], 1));
+console.log(nthElement2(['a', 'b', 'c'], 2));
+console.log(nthElement2(['a', 'b', 'c'], 4));
