95dcef566f39370a537abc4cc9654d0d9eb2230f
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-01-19 15:47:26 +0600

    solve the probelm : Write a JavaScript program to unflatten an object with the paths for keys.

--- w3School_Fundamental_Part_1/problem-36.js
+++ w3School_Fundamental_Part_1/problem-36.js
@@ -54,3 +54,56 @@ let funBegin = (number) => (number > 50 ? false : [-number, number + 10]);
 console.log(unfold1(funBegin, 10));
 let thirdFun = (number) => (number > 100 ? false : [number, number + number]);
 console.log(unfold1(thirdFun, 10));
+console.log('----------------------------------------------------------------');
+
+// task : Unflatten an object with the paths for keys.
+
+// point : solution 1 : using nested reduce and split(".")
+// / note : what is unflatten an object ?
+//  ans : A flattened object is one where nested properties are represented using a delimiter or a specific naming convention.
+
+const unflattenObject = (obj) =>
+	Object.keys(obj).reduce((acc, cur) => {
+		if (cur.indexOf('.') !== -1) {
+			const keys = cur.split('.');
+			Object.assign(
+				acc,
+				JSON.parse(
+					'{' +
+						keys
+							.map((value, index) =>
+								index !== keys.length - 1 ? `"${value} " : {` : `"${value}" : `,
+							)
+							.join('') +
+						obj[cur] +
+						'}'.repeat(keys.length),
+				),
+			);
+		} else acc[cur] = obj[cur];
+		return acc;
+	}, {});
+console.log(unflattenObject({ 'a.b.c': 1, d: 1 }));
+
+// point : using for loop instead of map
+const unflattenObject1 = (obj) =>
+	Object.keys(obj).reduce((acc, cur) => {
+		if (cur.indexOf('.') !== -1) {
+			const keys = cur.split('.');
+			let nestedObj = acc;
+
+			for (let i = 0; i < keys.length - 1; i++) {
+				if (!nestedObj[keys[i]]) {
+					nestedObj[keys[i]] = {};
+				}
+				nestedObj = nestedObj[keys[i]];
+			}
+
+			nestedObj[keys[keys.length - 1]] = obj[cur];
+		} else {
+			acc[cur] = obj[cur];
+		}
+
+		return acc;
+	}, {});
+
+console.log(JSON.stringify(unflattenObject1({ 'a.b.c': 1, d: 1 })));
