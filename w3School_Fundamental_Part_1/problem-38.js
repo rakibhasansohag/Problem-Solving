9e1f47a9cc9907bed73762884bde838bdade0aa6
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-02-08 01:31:50 +0600

    problem solve : Write a JavaScript program to convert a string to snake case.

--- w3School_Fundamental_Part_1/problem-38.js
+++ w3School_Fundamental_Part_1/problem-38.js
@@ -34,3 +34,42 @@ const tomorrow1 = (long = false) => {
 
 console.log(tomorrow1());
 console.log(tomorrow1(true));
+
+// TASK : Write a JavaScript program to convert a string to snake case.
+// Point : solution 1 : solve using regex pattern
+
+const toSnakeCase = (str) =>
+	str &&
+	str
+		.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+		.map((x) => x.toLowerCase())
+		.join('_');
+
+console.log(toSnakeCase('camelCase'));
+console.log(toSnakeCase('some text'));
+console.log(
+	toSnakeCase('some-mixed_string With spaces_underscores-and-hyphens'),
+);
+console.log(toSnakeCase('AllThe-small Things'));
+console.log(
+	toSnakeCase(
+		'IAmListeningToFMWhileLoadingDifferentURLOnMyBrowserAndAlsoEditingSomeXMLAndHTML',
+	),
+);
+
+// point : using replace
+const toSnakeCase1 = (str) => {
+	return str.replace(/[^a-zA-Z0-9]+(.)/g, (_, chr) => '_' + chr.toLowerCase());
+};
+
+console.log(toSnakeCase1('camelCase'));
+console.log(toSnakeCase1('some text'));
+console.log(
+	toSnakeCase1('some-mixed_string With spaces_underscores-and-hyphens'),
+);
+console.log(toSnakeCase1('AllThe-small Things'));
+console.log(
+	toSnakeCase1(
+		'IAmListeningToFMWhileLoadingDifferentURLOnMyBrowserAndAlsoEditingSomeXMLAndHTML',
+	),
+);
