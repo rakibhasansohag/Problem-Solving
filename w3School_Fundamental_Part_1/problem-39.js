f0d7996fe6787870762da9341a56b5818f72e6ec
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-02-13 13:02:08 +0600

    problem solve : Write a JavaScript program to reduce a given Array-like into a value hash (keyed data store).

--- w3School_Fundamental_Part_1/problem-39.js
+++ w3School_Fundamental_Part_1/problem-39.js
@@ -115,3 +115,36 @@ console.log(
 		'IAmListeningToFMWhileLoadingDifferentURLOnMyBrowserAndAlsoEditingSomeXMLAndHTML',
 	),
 );
+
+console.log('----------------------------------------------------------------');
+// task : Write a JavaScript program to reduce a given Array-like into a value hash (keyed data store).
+
+const toHash = (object, key) => {
+	return Array.prototype.reduce.call(
+		object,
+		(acc, data, index) => {
+			acc[!key ? index : data[key]] = data;
+			return acc;
+		},
+		{},
+	);
+};
+
+console.log(toHash([4, 3, 2, 1])); // {0: 4, 1: 3, 2: 2, 3: 1}
+
+console.log(toHash([{ a: 'label' }, 'a'])); // {label: {a: 'label}}
+let users = [
+	{ id: 1, first: 'Jon' },
+	{ id: 2, first: 'Joe' },
+	{ id: 3, first: 'Moe' },
+];
+let managers = [{ manager: 1, employees: [2, 3] }];
+
+managers.forEach(
+	(manager) =>
+		(manager.employees = manager.employees.map(function (id) {
+			return this[id];
+		}, toHash(users, 'id'))),
+);
+
+console.log(managers);
