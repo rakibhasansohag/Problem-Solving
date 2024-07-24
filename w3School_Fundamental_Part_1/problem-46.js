ae1102f25ed19904db1ef9b2c8a6c002189507d1
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-07-24 21:14:58 +0600

    Mutate the original array to filter out the values specified, based on a given iterator function

--- w3School_Fundamental_Part_1/problem-46.js
+++ w3School_Fundamental_Part_1/problem-46.js
@@ -34,4 +34,58 @@ const readFileLines = (filename) =>
 	fs.readFileSync(filename).toString('UTF-8').split('\n');
 
 const textArr = readFileLines('problem-1.js');
-console.log(textArr);
+// console.log(textArr);
+
+// Task : Mutate the original array to filter out the values specified, based on a given iterator function.
+// Point : solution 1 : using filter function
+const pullBy = (arr, ...args) => {
+	const length = args.length;
+
+	let fn = length > 1 ? args[length - 1] : undefined;
+	fn = typeof fn == 'function' ? (args.pop(), fn) : undefined;
+
+	let argState = (Array.isArray(args[0]) ? args[0] : args).map((val) =>
+		fn(val),
+	);
+
+	let pulled = arr.filter((v, i) => !argState.includes(fn(v)));
+
+	arr.length = 0;
+	pulled.forEach((v) => arr.push(v));
+};
+
+var myArray = [{ x: 1 }, { x: 2 }, { x: 3 }, { x: 5 }];
+const filterArray = [{ x: 2 }, { x: 1 }];
+console.log('my array 1', myArray);
+// pullBy(myArray, filterArray, (o) => o.x);
+console.log(myArray);
+
+// Point : solution 2 : without using any built-in function
+const pullBy1 = (arr, ...args) => {
+	const length = args.length;
+
+	let fn = length > 1 ? args[length - 1] : undefined;
+	fn = typeof fn == 'function' ? (args.pop(), fn) : undefined;
+
+	let argState = [];
+	if (Array.isArray(args[0])) {
+		for (let i = 0; i < args[0].length; i++) {
+			argState.push(fn(args[0][i]));
+		}
+	} else {
+		for (let i = 0; i < args.length; i++) {
+			argState.push(fn(args[i]));
+		}
+	}
+
+	let i = 0;
+	while (i < arr.length) {
+		if (argState.includes(fn(arr[i]))) {
+			arr.splice(i, 1);
+		} else {
+			i++;
+		}
+	}
+};
+pullBy1(myArray, filterArray, (x) => x.x);
+console.log(myArray);
