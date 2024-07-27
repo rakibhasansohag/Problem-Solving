e9fdbae1b94277cb78948ab030bc31bc6291b143
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-07-27 14:53:27 +0600

    probelm solve : Calculate how many numbers in the given array are less or equal to the given value using the percentile formula

---
+++ w3School_Fundamental_Part_1/problem-48.js
@@ -0,0 +1,34 @@
+'use strict';
+console.log('working...');
+
+// Task : Calculate how many numbers in the given array are less or equal to the given value using the percentile formula.
+
+// Point : solution 1 : using filter
+const percentile = (arr, num) =>
+	(arr.filter((item) => item <= num).length / arr.length) * 100;
+
+console.log(percentile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5));
+console.log(percentile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10));
+console.log(percentile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0));
+console.log(percentile([1, 2, 3, 4, 5, 6], 4).toFixed(2)); // 66.66;
+
+const percentile1 = (arr, num) => {
+	let count = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] < num) {
+			count++;
+		}
+
+		if (arr[i] === num) {
+			return (count / arr.length) * 100;
+		}
+	}
+
+	return (count / arr.length) * 100;
+};
+
+console.log(percentile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5));
+console.log(percentile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10));
+console.log(percentile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0));
+console.log(percentile([1, 2, 3, 4, 5, 6], 4).toFixed(2)); // 66.66;
