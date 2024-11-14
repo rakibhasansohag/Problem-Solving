aaf1e8c073e0aff2b853b47b2fbaf5fd9b272c76
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-11-14 23:21:18 +0600

    knowing about Big O notation

--- JavaScript-Algorithms-and-Data-Structures/big-o/problem-1.js
+++ JavaScript-Algorithms-and-Data-Structures/big-o/problem-1.js
@@ -19,8 +19,37 @@ function addUpTo1(n) {
 	return (n * (n + 1)) / 2;
 }
 
-let t3 = performance.now();
-console.log(addUpTo1(10));
-let t4 = performance.now();
-console.log(`Time elapsed: ${(t4 - t3) / 1000} seconds`);
+// let t3 = performance.now();
 // console.log(addUpTo1(10));
+// let t4 = performance.now();
+// console.log(`Time elapsed: ${(t4 - t3) / 1000} seconds`);
+// console.log(addUpTo1(10));
+
+
+// Note: Big O
+
+// Point: Problem 1) TODO: countUpAndCountDown
+function countUpAndCountDown(n) {
+	console.log('Counting up');
+	for (let i = 0 ; i< n; i ++) {
+		console.log(i);
+	}
+	console.log("At the top!\nGoing Down...")
+	for (let j = n -1; j>= 0 ;j --){
+		console.log(j)
+	}
+	console.log('Back down!')
+}
+
+// countUpAndCountDown(10)
+
+// Point: Problem 2) TODO: PrintAllPairs
+function printAllPairs(n){
+	for (let i = 0 ; i < n; i ++){
+		for (let j = 0 ; j < n ; j ++) {
+			console.log(i,j)
+		}
+	}
+}
+
+// printAllPairs(10)
