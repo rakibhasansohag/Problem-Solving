484258e624900af72b1fd0b689c48d5e5cffa582
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-11-26 22:19:44 +0600

    char count code refector

--- JavaScript-Algorithms-and-Data-Structures/algorithms/problem-1.js
+++ JavaScript-Algorithms-and-Data-Structures/algorithms/problem-1.js
@@ -40,3 +40,14 @@ function charCount(str) {
 
 console.log(charCount('aabb'));
 console.log(charCount('hello world!'));
+
+
+// Point: Refactoring the char count function
+function refactorCharCount(str){
+	var obj = {};
+	for(var char of str){
+		if (/[a-z0-9]/.test(char)) obj[char] = ++obj[char] || 1;
+	}
+	return obj
+}
+
