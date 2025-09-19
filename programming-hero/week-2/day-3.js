4ff7874e490bc98932a65cec1a675d74dbb8d89a
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2025-09-19 14:37:28 +0600

    day 3 of problem solving

--- programming-hero/week-2/day-3.js
+++ programming-hero/week-2/day-3.js
@@ -38,3 +38,45 @@ function isPositive(a) {
 }
 
 isPositive(3);
+
+/**
+ * Function Description
+
+Complete the getSecondLargest function in the editor below.
+
+getSecondLargest has the following parameters:
+
+int nums[n]: an array of integers
+Returns
+
+int: the second largest number in 
+Input Format
+
+The first line contains an integer, , the size of the  array.
+The second line contains  space-separated numbers that describe the elements in .
+
+Constraints
+
+, where  is the number at index .
+The numbers in  may not be distinct.
+ * Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+
+ */
+
+function getSecondLargest(nums) {
+	// Complete the function
+	let sorted = nums.sort ((a,b) => b-a)
+
+	let largest = sorted[0]
+
+	for (let i = 0 ; i < sorted.length ; i ++) {
+		if (sorted[i] < largest) {
+			return sorted[i]
+		}
+	}
+	return null
+}
+
+console.log(getSecondLargest([1, 2, 3, 4, 6,6,5]))
