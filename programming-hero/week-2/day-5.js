7ec530a99082dade9f6e3d63727d8717e000aef0
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2025-09-20 21:39:21 +0600

    day 5 modify array problem solved

--- programming-hero/week-2/day-5.js
+++ programming-hero/week-2/day-5.js
@@ -106,3 +106,50 @@ function sides(literals, ...expressions) {
 	return [s1, s2].sort((a,b) => a - b);
 }
 
+
+/**
+ * 
+ * Task
+
+Complete the function in the editor. It has one parameter: an array, . It must iterate through the array performing one of the following actions on each element:
+
+If the element is even, multiply the element by .
+If the element is odd, multiply the element by .
+The function must then return the modified array.
+
+Input Format
+
+The first line contains an integer, , denoting the size of .
+The second line contains  space-separated integers describing the respective elements of .
+
+Constraints
+
+, where  is the  element of .
+Output Format
+
+Return the modified array where every even element is doubled and every odd element is tripled.
+* 
+*/
+
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ * 
+ */
+
+function modifyArray(nums) {
+	
+	for (let i = 0 ; i < nums.length ; i ++) {
+		if (nums[i] % 2 === 0) {
+			nums[i] *= 2;
+		} else {
+			nums[i] *= 3;
+		}
+	}
+	return nums
+
+}
+
+
